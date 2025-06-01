1da3b955bf01b9d0746b2e81f9a03a523a9eef7e
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-06-01 23:48:35 +0100

    setting up the routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,3 +2,6 @@ import express from "express"
 
 const router = express.Router()
 
+router.post("/api/auth/register",(req,res)=>{
+  res.status(200).send("register user")
+})
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,7 @@
-console.log("Nodejs on termux")
+import express from "express"
+
+const app = express()
+
+app.listen(8080,()=>{
+  console.log("Server up and running on port 8080")
+})
