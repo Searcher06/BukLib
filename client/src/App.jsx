767d97ee4a8bed431e52f9065ab64e17f97d56fe
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-06-01 01:29:05 +0100

    Initial commit 🚀

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='w-50 h-50 bg-amber-500'>
+    Hello worl
+    </div>
+    </>
+  )
+}
+
+export default App
